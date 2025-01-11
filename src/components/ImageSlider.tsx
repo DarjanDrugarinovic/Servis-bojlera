ebf37bfaaaef26d768ee118b8b3e516a0c6dcd5c
JasminaTurku <JasminaT@elfak.rs>
2025-01-11 11:23:41 +0100

    servis bojlera

---
+++ src/components/ImageSlider.tsx
@@ -0,0 +1,47 @@
+import { useState, useEffect } from 'react';
+import slika1 from '../assets/ariston.jpg'; 
+import slika2 from '../assets/protocni-bojler-bosch.jpg' 
+import slika3 from '../assets/protocni-bojler-simens.jpg'
+import slika4 from '../assets/servis-bojlera-bosch.jpg'
+import slika5 from '../assets/servis-bojlera-dom-pancevo.jpg'
+import slika6 from '../assets/servis-bojlera-elit-inox.jpg'
+import slika7 from '../assets/servis-bojlera-elmac.jpg'
+import slika8 from '../assets/servis-bojlera-gorenje.jpg'
+import slika9 from '../assets/servis-bojlera-koncar.jpg'
+import slika10 from '../assets/servis-bojlera-magnohrom.jpg'
+import slika11 from '../assets/servis-bojlera-metalac.jpg'
+import slika12 from '../assets/servis-bojlera-rankom.jpg'
+import slika13 from '../assets/servis-bojlera-termorad.jpg'
+import slika14 from '../assets/servis-bojlera-tesy.jpg'
+import styled from 'styled-components';
+
+
+const images = [slika1, slika2, slika3, slika4, slika5, slika6, slika7, slika8, slika9, slika10, slika11, slika12, slika13, slika14];
+
+const ImageSlider = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 1500);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div style={{ position: 'relative', maxWidth: '600px', margin: '0 auto' }}>
+      <img
+        src={images[currentIndex]}
+        alt={`Slide ${currentIndex + 1}`}
+        style={{ width: '100%', display: 'block' }}
+      />
+    </div>
+  );
+};
+
+const MainDiv= styled.div`
+
+position: 'relative', maxWidth: '600px', margin: '0 auto'
+`
+export default ImageSlider;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,3 +1,4 @@
+import ImageSlider from "components/ImageSlider";
 import { Layout } from "components/Layout";
 import styled from "styled-components";
 
@@ -16,6 +17,7 @@ const Home = () => {
         <PTel>00-24</PTel>
         <Hr />
       </MainDiv>
+      <ImageSlider/>
     </Layout>
   );
 };
